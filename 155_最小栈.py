fda83a53cdfeda05fe15a44c774a9d9e38caf524
sunzhiyu <sunzhiyu94@163.com>
2019-11-08 16:50:37 +0800

    feature: 最小栈

---
+++ 155_最小栈.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 个人解法，效率有待提高
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        min = None
+        for i in self.stack:
+            if min is None or i < min:
+                min = i
+        return min
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+# 民间解答
+# 1. 辅助栈和数据栈同步
+class MinStack1:
+
+    # 辅助栈和数据栈同步
+    # 思路简单不容易出错
+
+    def __init__(self):
+        # 数据栈
+        self.data = []
+        # 辅助栈
+        self.helper = []
+
+    def push(self, x):
+        self.data.append(x)
+        if len(self.helper) == 0 or x <= self.helper[-1]:
+            self.helper.append(x)
+        else:
+            self.helper.append(self.helper[-1])
+
+    def pop(self):
+        if self.data:
+            self.helper.pop()
+            return self.data.pop()
+
+    def top(self):
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self):
+        if self.helper:
+            return self.helper[-1]
+
+
+# 2. 辅助栈和数据栈不同步
+class MinStack2:
+
+    # 辅助栈和数据栈不同步
+    # 关键 1：辅助栈的元素空的时候，必须放入新进来的数
+    # 关键 2：新来的数小于或者等于辅助栈栈顶元素的时候，才放入（特别注意这里等于要考虑进去）
+    # 关键 3：出栈的时候，辅助栈的栈顶元素等于数据栈的栈顶元素，才出栈，即"出栈保持同步"就可以了
+
+    def __init__(self):
+        # 数据栈
+        self.data = []
+        # 辅助栈
+        self.helper = []
+
+    def push(self, x):
+        self.data.append(x)
+        # 关键 1 和关键 2
+        if len(self.helper) == 0 or x <= self.helper[-1]:
+            self.helper.append(x)
+
+    def pop(self):
+        # 关键 3：【注意】不论怎么样，数据栈都要 pop 出元素
+        top = self.data.pop()
+
+        if self.helper and top == self.helper[-1]:
+            self.helper.pop()
+        return top
+
+    def top(self):
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self):
+        if self.helper:
+            return self.helper[-1]
+
+
+# 我的个人优化版
+class MyMinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.data = []
+        self.helper = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.data.append(x)
+        if not self.helper or x < self.helper[-1]:
+            self.helper.append(x)
+        else:
+            self.helper.append(self.helper[-1])
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.data.pop()
+        self.helper.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.data[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.helper[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
