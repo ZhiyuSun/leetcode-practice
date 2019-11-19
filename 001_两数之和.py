80ce7f27b0708a3aea016baf7a6c3438ea348ac4
ZhiyuSun <zcgyxlgy@sina.com>
2019-11-19 23:01:23 +0800

    feature: 两数之和

---
+++ 001_两数之和.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 第一版，我写了一个返回值的版本，因为字节面试的时候遇到了
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums or len(nums) == 1:
+            return None
+        nums.sort()
+        i, j = 0, len(nums)-1
+        while i < j:
+            result = nums[i] + nums[j]
+            if result == target:
+                return [nums[i], nums[j]]
+            elif result < target:
+                i += 1
+            else:
+                j -= 1
+        return None
+
+# 第二版，暴力法。时间复杂度O(n2),空间复杂度O(1)
+class Solution1(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(0, len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                j += 1
+        return None
