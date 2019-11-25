b85e0a0de23fe4bdaf29c22efc4f8ebb6a8cb73c
ZhiyuSun <zcgyxlgy@sina.com>
2019-11-25 20:06:58 +0800

    feature: 存在重复元素

---
+++ 217_存在重复元素.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) < len(nums)
