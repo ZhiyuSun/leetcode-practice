735308fd2b3ad3e1d8aadc81502616c61932fde5
ZhiyuSun <zcgyxlgy@sina.com>
2020-07-05 23:17:43 +0800

    二叉树的心得

--- 101-200/102_二叉树的层序遍历.py
+++ 101-200/102_二叉树的层序遍历.py
@@ -84,3 +84,30 @@ class Solution2(object):
             level = next_level
         return result
                 
+
+class Solution3(object):
+	def levelOrder(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[List[int]]
+		"""
+		if not root:
+			return []
+		res = []
+		queue = [root]
+		while queue:
+			# 获取当前队列的长度，这个长度相当于 当前这一层的节点个数
+			size = len(queue)
+			tmp = []
+			# 将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
+			# 如果节点的左/右子树不为空，也放入队列中
+			for _ in range(size):
+				r = queue.pop(0)
+				tmp.append(r.val)
+				if r.left:
+					queue.append(r.left)
+				if r.right:
+					queue.append(r.right)
+			# 将临时list加入最终返回结果中
+			res.append(tmp)
+		return res
