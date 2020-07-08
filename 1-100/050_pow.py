873abc22d6f8977fd695d3443633ac00ef5bdb45
ZhiyuSun <zcgyxlgy@sina.com>
2020-07-08 23:49:27 +0800

    上传一些题

---
+++ 1-100/050_pow.py
@@ -0,0 +1,9 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def quickMul(N):
+            if N == 0:
+                return 1.0
+            y = quickMul(N // 2)
+            return y * y if N % 2 == 0 else y * y * x
+        
+        return quickMul(n) if n >= 0 else 1.0 / quickMul(-n)
