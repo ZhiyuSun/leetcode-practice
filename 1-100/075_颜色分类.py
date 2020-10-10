b9bcd164ed30b4922100169e21843b22b8c3223b
ZhiyuSun <zcgyxlgy@sina.com>
2020-10-10 21:53:53 +0800

    颜色分类

---
+++ 1-100/075_颜色分类.py
@@ -0,0 +1,65 @@
+"""
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+注意:
+不能使用代码库中的排序函数来解决这道题。
+
+示例:
+
+输入: [2,0,2,1,1,0]
+输出: [0,0,1,1,2,2]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-colors
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# 2020.10.10 慢慢恢复
+# 单指针
+from typing import List
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        n = len(nums)
+        ptr = 0
+        for i in range(n):
+            if nums[i] == 0:
+                nums[i], nums[ptr] = nums[ptr], nums[i]
+                ptr += 1
+        for i in range(ptr, n):
+            if nums[i] == 1:
+                nums[i], nums[ptr] = nums[ptr], nums[i]
+                ptr += 1
+
+# 双指针
+class Solution2:
+    def sortColors(self, nums: List[int]) -> None:
+        n = len(nums)
+        p0 = p1 = 0
+        for i in range(n):
+            if nums[i] == 1:
+                nums[i], nums[p1] = nums[p1], nums[i]
+                p1 += 1
+            elif nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                if p0 < p1:
+                    nums[i], nums[p1] = nums[p1], nums[i]
+                p0 += 1
+                p1 += 1
+
+# 双指针2
+class Solution3:
+    def sortColors(self, nums: List[int]) -> None:
+        n = len(nums)
+        p0, p2 = 0, n - 1
+        i = 0
+        while i <= p2:
+            while i <= p2 and nums[i] == 2:
+                nums[i], nums[p2] = nums[p2], nums[i]
+                p2 -= 1
+            if nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                p0 += 1
+            i += 1
+
