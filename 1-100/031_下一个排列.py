db900e46e0958711a80981a1804d2aa5bd12088e
ZhiyuSun <zcgyxlgy@sina.com>
2020-11-11 00:02:04 +0800

    下一个排列

---
+++ 1-100/031_下一个排列.py
@@ -0,0 +1,34 @@
+"""
+实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+
+如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+
+必须原地修改，只允许使用额外常数空间。
+
+以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/next-permutation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# 2020.11.10 又是直奔题解的一天
+from typing import List
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[i] >= nums[j]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
---
+++ 101-200/143_重排链表.py
@@ -0,0 +1,47 @@
+"""
+给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+示例 1:
+
+给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+示例 2:
+
+给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reorder-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        if not head:
+            return
+        
+        vec = list()
+        node = head
+        while node:
+            vec.append(node)
+            node = node.next
+        
+        i, j = 0, len(vec) - 1
+        while i < j:
+            vec[i].next = vec[j]
+            i += 1
+            if i == j:
+                break
+            vec[j].next = vec[i]
+            j -= 1
+        
+        vec[i].next = None
+
+
+
