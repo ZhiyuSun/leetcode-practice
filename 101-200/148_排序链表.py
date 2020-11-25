5c0b2080e5440d0b7400c60dd608d625094cc531
ZhiyuSun <zcgyxlgy@sina.com>
2020-11-25 21:21:28 +0800

    算法练习

--- 101-200/148_排序链表.py
+++ 101-200/148_排序链表.py
@@ -6,3 +6,44 @@
 输入: 4->2->1->3
 输出: 1->2->3->4
 """
+
+# 2020.11.25 自上而下+归并，直奔题解
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def sortFunc(head: ListNode, tail: ListNode) -> ListNode:
+            if not head:
+                return head
+            if head.next == tail:
+                head.next = None
+                return head
+            slow = fast = head
+            while fast != tail:
+                slow = slow.next
+                fast = fast.next
+                if fast != tail:
+                    fast = fast.next
+            mid = slow
+            return merge(sortFunc(head, mid), sortFunc(mid, tail))
+            
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+        
+        return sortFunc(head, None)
---
+++ 算法面经整理/leetcode面经.md
@@ -0,0 +1,2 @@
+https://leetcode-cn.com/circle/discuss/93U8ox/
+https://leetcode-cn.com/circle/discuss/BOLS8z/
