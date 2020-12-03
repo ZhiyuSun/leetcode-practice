41ca1d31a1ef5afc09c9c427adb26610c75fdd03
ZhiyuSun <zcgyxlgy@sina.com>
2020-12-03 22:59:31 +0800

    二叉树公共祖先

--- 剑指Offer/68_二叉树的最近公共祖先.py
+++ 剑指Offer/68_二叉树的最近公共祖先.py
@@ -22,13 +22,11 @@ class TreeNode:
         self.left = None
         self.right = None
 
-
+# 2020.12.3 复习一下
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root or root == p or root == q: return root
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
-        if not left and not right: return
-        if not left: return right
-        if not right: return left
-        return root
+        if left and right: return root
+        return left if left else right
