a7bcfb63418b61be3a784310276a3df2d7e564d9
ZhiyuSun <zcgyxlgy@sina.com>
2020-12-20 20:53:10 +0800

    去除重复字母

---
+++ 301-500/316_去除重复字母.py
@@ -0,0 +1,32 @@
+"""
+给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+
+注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters 相同
+
+ 
+
+示例 1：
+
+输入：s = "bcabc"
+输出："abc"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicate-letters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import collections
+
+# 2020.12.20 直奔题解
+class Solution:
+    def removeDuplicateLetters(self, s) -> int:
+        stack = []
+        remain_counter = collections.Counter(s)
+
+        for c in s:
+            if c not in stack:
+                while stack and c < stack[-1] and  remain_counter[stack[-1]] > 0:
+                    stack.pop()
+                stack.append(c)
+            remain_counter[c] -= 1
+        return ''.join(stack)
+
