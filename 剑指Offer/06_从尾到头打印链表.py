33ff88e3bd46af162f8f93670809c98d9355f44c
ZhiyuSun <zcgyxlgy@sina.com>
2020-12-24 21:03:54 +0800

    找软柿子捏

---
+++ 剑指Offer/06_从尾到头打印链表.py
@@ -0,0 +1,27 @@
+"""
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+"""
+from typing import List
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        return self.reversePrint(head.next) + [head.val] if head else []
+
+
+class Solution1:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+        return stack[::-1]
