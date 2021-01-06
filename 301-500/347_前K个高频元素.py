02c96ff07302329802fc73f2fbadefa8c6b5dfa6
ZhiyuSun <zcgyxlgy@sina.com>
2021-01-06 22:59:30 +0800

    我是一个垃圾

--- 301-500/347_前K个高频元素.py
+++ 301-500/347_前K个高频元素.py
@@ -1,5 +1,21 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+"""
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+
+ 
+
+示例 1:
+
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+示例 2:
+
+输入: nums = [1], k = 1
+输出: [1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
 import collections, heapq
 class Solution(object):
     def topKFrequent(self, nums, k):
@@ -10,3 +26,16 @@ class Solution(object):
         """ 
         count = collections.Counter(nums)   
         return heapq.nlargest(k, count.keys(), key=count.get) 
+
+# 2021.01.06 这题很经典，但是我还是不会
+from typing import List
+class Solution1:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = collections.Counter(nums)
+        heap, ans = [], []
+        for i in dic:
+            heapq.heappush(heap, (-dic[i], i))
+        for _ in range(k):
+            ans.append(heapq.heappop(heap)[1])
+        return ans
+
