ed8883fab640606698c2e2ee49e8eadb93f4bbd9
ZhiyuSun <zcgyxlgy@sina.com>
2021-01-13 22:59:52 +0800

    排序算法

--- 501-1000/912_排序数组.py
+++ 501-1000/912_排序数组.py
@@ -16,7 +16,8 @@
 链接：https://leetcode-cn.com/problems/sort-an-array
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 """
-
+import random
+from typing import List
 
 class Solution(object):
     def sortArray(self, nums):
@@ -177,3 +178,55 @@ class Solution(object):
             less = [x for x in nums[1:] if x < pivot]
             greater = [x for x in nums[1:] if x >= pivot]
             return self.quicksort2(less) + [pivot] + self.quicksort2(greater)
+
+    def randomized_partition(self, nums, l, r):
+        pivot = random.randint(l, r)
+        nums[pivot], nums[r] = nums[r], nums[pivot]
+        i = l - 1
+        for j in range(l, r):
+            if nums[j] < nums[r]:
+                i += 1
+                nums[j], nums[i] = nums[i], nums[j]
+        i += 1
+        nums[i], nums[r] = nums[r], nums[i]
+        return i
+
+    def randomized_quicksort(self, nums, l, r):
+        if r - l <= 0:
+            return
+        mid = self.randomized_partition(nums, l, r)
+        self.randomized_quicksort(nums, l, mid - 1)
+        self.randomized_quicksort(nums, mid + 1, r)
+
+    def quick_sort3(self, nums: List[int]) -> List[int]:
+        self.randomized_quicksort(nums, 0, len(nums) - 1)
+        return nums
+
+
+    # def max_heapify(self, heap, root, heap_len):
+    #     p = root
+    #     while p * 2 + 1 < heap_len:
+    #         l, r = p * 2 + 1, p * 2 + 2
+    #         if heap_len <= r or heap[r] < heap[l]:
+    #             nex = l
+    #         else:
+    #             nex = r
+    #         if heap[p] < heap[nex]:
+    #             heap[p], heap[nex] = heap[nex], heap[p]
+    #             p = nex
+    #         else:
+    #             break
+        
+    # def build_heap(self, heap):
+    #     for i in range(len(heap) - 1, -1, -1):
+    #         self.max_heapify(heap, i, len(heap))
+
+    # def heap_sort(self, nums):
+    #     self.build_heap(nums)
+    #     for i in range(len(nums) - 1, -1, -1):
+    #         nums[i], nums[0] = nums[0], nums[i]
+    #         self.max_heapify(nums, 0, i)
+            
+    # def sortArray(self, nums: List[int]) -> List[int]:
+    #     self.heap_sort(nums)
+    #     return nums
