886c71a63449c6eb5d828f7134986ad21555d8f9
ZhiyuSun <zcgyxlgy@sina.com>
2021-01-14 22:48:01 +0800

    排序算法

--- 1-100/088_合并两个有序数组.py
+++ 1-100/088_合并两个有序数组.py
@@ -1,3 +1,13 @@
+"""
+给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 有足够的空间（空间大小等于 m + n）来保存 nums2 中的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
@@ -97,3 +107,26 @@ class Solution3(object):
         
         # add missing elements from nums2
         nums1[:p2 + 1] = nums2[:p2 + 1]
+
+# 2021.01.14 调试了好久才出来，差劲
+from typing import List
+class Solution4:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if m == 0: 
+            nums1[:] = nums2
+            return
+        i, j, k = m-1, n-1, len(nums1)-1
+        while i >= 0 and j >=0:
+            if nums1[i] < nums2[j]:
+                nums1[k] = nums2[j]
+                j -= 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
+            k -= 1
+        if j >= 0:
+            nums1[0:k+1] = nums2[0:k+1]
+        return nums1
--- 201-300/215_数组中的第K个最大元素.py
+++ 201-300/215_数组中的第K个最大元素.py
@@ -1,3 +1,20 @@
+"""
+在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+示例 1:
+
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
+示例 2:
+
+输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import heapq
@@ -10,3 +27,76 @@ class Solution:
         :rtype: int
         """
         return heapq.nlargest(k, nums)[-1]
+
+# 2021.01.14 学习leetcode上的解法
+from typing import List
+
+# 暴力排序
+class Solution1:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        size = len(nums)
+        nums.sort()
+        return nums[size - k]
+
+# 快速排序
+import random
+
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        size = len(nums)
+
+        target = size - k
+        left = 0
+        right = size - 1
+        while True:
+            index = self.__partition(nums, left, right)
+            if index == target:
+                return nums[index]
+            elif index < target:
+                # 下一轮在 [index + 1, right] 里找
+                left = index + 1
+            else:
+                right = index - 1
+
+    #  循环不变量：[left + 1, j] < pivot
+    #  (j, i) >= pivot
+    def __partition(self, nums, left, right):
+        # 随机化切分元素
+        # randint 是包括左右区间的
+        # random_index = random.randint(left, right)
+        # nums[random_index], nums[left] = nums[left], nums[random_index]
+
+        pivot = nums[left]
+        j = left
+        for i in range(left + 1, right + 1):
+            if nums[i] < pivot:
+                j += 1
+                nums[i], nums[j] = nums[j], nums[i]
+
+        nums[left], nums[j] = nums[j], nums[left]
+        return j
+
+# 优先队列
+class Solution3:
+
+    # 使用容量为 k 的小顶堆
+    # 元素个数小于 k 的时候，放进去就是了
+    # 元素个数大于 k 的时候，小于等于堆顶元素，就扔掉，大于堆顶元素，就替换
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        size = len(nums)
+        if k > size:
+            raise Exception('程序出错')
+
+        L = []
+        for index in range(k):
+            # heapq 默认就是小顶堆
+            heapq.heappush(L, nums[index])
+
+        for index in range(k, size):
+            top = L[0]
+            if nums[index] > top:
+                # 看一看堆顶的元素，只要比堆顶元素大，就替换堆顶元素
+                heapq.heapreplace(L, nums[index])
+        # 最后堆顶中的元素就是堆中最小的，整个数组中的第 k 大元素
+        return L[0]
